70ad103d5a79cf19b035312c573c8616987688d6
igor-borisoglebski <igorborisoglebski@gmail.com>
2022-03-16 21:49:16 +0000

    make test update: more ram

--- scripts/test-image.sh
+++ scripts/test-image.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-qemu-system-x86_64 -m 256 -cdrom ../alpine-kvmhost-latest-x86_64.iso --nographic
+qemu-system-x86_64 -m 1024 -cdrom alpine-kvmhost-latest-x86_64.iso --nographic #yes, 1024 :O
